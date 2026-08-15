2d7d24e74ac87b4fe6ce2fcda47fefaaa84775bf
Simpolette <84978940+Simpolette@users.noreply.github.com>
2026-08-15 21:09:22 +0700

    fix: 🚀 upgrade Node.js to version 22 for Astro 5 build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://yuran1811.github.io',
+  site: 'https://Simpolette.github.io',
   base: '/jamstack-demo/',
   output: 'static',
 });
